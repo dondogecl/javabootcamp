be703d5499b7a527c89c175e3e719c958c6abf41
Alejandro A <ale@doges.dev>
2022-08-09 15:30:35 -0400

    modified gitignore, added new exercises

---
+++ DocDocuments.java
@@ -0,0 +1,37 @@
+public class DocDocuments {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+     * Function name: greet
+     * 
+     * Inside the function
+     * prints: 'Hello, adventurer <name>!'
+     */
+
+    public static void greet(){
+
+    }
+
+    /*
+    * Function name: printText
+    * param: name (String)
+    * param: age (int)
+    */
+    public static void printText(String name, int age){
+        System.out.println("Hello, adventurer " + name + "!");
+        System.out.println("You are " + age + " years old.");
+    }
+
+    /*
+     * Function name: calculateArea
+     * @param: length (double)
+     * @param: width (double)
+     * @return: area (double)
+     */
+    public static double calculateArea(double length, double width){
+        double area = 0;
+        return area;
+    }
+}
